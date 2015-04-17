96930f3ea48cda4f3f2223dba81d274cdefc4a07
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-17 10:51:08 -0700

    got server to work and moved files to public dir

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
-'use strict'
+var express = require('express'); /*iife not needed because Node does it
+by default  --any time you're getting something from another file, you'll
+use the 'require' statement. Require says 'go get this from another file'
+add node_modules to your git ignore.  */
 
-var http = require('http');
-var express = require('express');
+var app = express();
 
-//404 response
-function send404Response(response) {
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: Page not found!');
-  response.end();
-}
+app.use(express.static(__dirname + '/public')); //addresses that begin with / are absolute paths (as opposed to relative)
 
-//Handle a user request
-function onRequest(request, response) {
+app.get('/*', function(req, res) {
+  res.status(404).send('could not find page');
+});
 
-  if (request.method == 'Get' && request.url == '/') {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    express.createReadStream('./index.html').pipe(response);
-  }else {
-    send404Response(response);
-  }
-}
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server running');
+});
 
-http.createServer(onRequest).listen(8888);
-console.log('Server is now running....');
 
 //'use strict'
 //
-//var express = require('express') //require is used if you need data from another file
+////var http = require('http');
 //
+///*iife not needed because Node does it by default  --any time
+//you're getting something from another file, you'll use the
+//'require' statement. Require says 'go get this from another file'
+//add node_modules to your git ignore.  */
+//var express = require('express');
 //var app = express();
 //
-//app.use(express.static('/public'));
+//app.use(express.static(__dirname + '/'));
 //
-//app.listen
+////404 response
+//function send404Response(response) {
+//  response.writeHead(404, {'Content-Type': 'text/plain'});
+//  response.write('Error 404: Page not found!');
+//  response.end();
+//}
+//
+////Handle a user request
+//function onRequest(request, response) {
+//
+//  if (request.method == 'Get' && request.url == '/') {
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    express.createReadStream('./index.html').pipe(response);
+//  }else {
+//    send404Response(response);
+//  }
+//}
+//
+//http.createServer(onRequest).listen(3000);
+//console.log('Server is now running....');
